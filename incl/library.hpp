e29a786377c089ef63a0266cb1e24214d9bc4885
Slampine <sakari.lampinen@gmail.com>
2024-06-12 13:44:14 +0300

    linux migration

--- incl/library.hpp
+++ incl/library.hpp
@@ -1,22 +1,24 @@
 #ifndef LIBRARY_HPP
 # define LIBRARY_HPP
+# include <array>
+# include <errno.h>
+# include <fcntl.h>
+# include <fstream>
+# include <iomanip>
+# include <iostream>
+# include <map>
+# include <netinet/in.h>
+# include <poll.h>
+# include <sstream>
 # include <stdio.h>
+# include <stdlib.h>
+# include <string>
+# include <string.h>
 # include <sys/socket.h>
 # include <sys/types.h>
-# include <unistd.h>
-# include <stdlib.h>
-# include <netinet/in.h>
-# include <iostream>
-# include <string>
-# include <fcntl.h>
-# include <vector>
-# include <sstream>
-# include <fstream>
+# include <sys/wait.h>
 # include <time.h>
-# include <iomanip>
-# include <fstream>
-# include <poll.h>
-# include <map>
-# include <array>
+# include <unistd.h>
+# include <vector>
 
 #endif
