7e961400ef7eaec585914ecee2278d8ed55f4245
nicktvdd <nicktvdd@gmail.com>
2024-06-25 22:07:16 +0300

    trying some things

--- src/Manager-Client.cpp
+++ src/Manager-Client.cpp
@@ -21,27 +21,20 @@ void Manager::handleClientCommunication(size_t index)
 {
 	char buffer[1024];
 	int bytesReceived = recv(fds[index].fd, buffer, sizeof(buffer), 0);
-	if (bytesReceived < 0)
+	if (bytesReceived == -1)
 	{
 		closeConnections(index, "Recv error: " + std::string(strerror(errno)));
 		return;
 	}
 	else if (bytesReceived == 0)
 	{
-		// Connection closed by client
-		std::cout << std::endl
-				  << "Client closed connection" << std::endl;
-		close(fds[index].fd);
-		fds.erase(fds.begin() + index);
-		fdsTimestamps.erase(fdsTimestamps.begin() + index);
-		cgiOnGoing.erase(cgiOnGoing.begin() + index);
-		index--; // Adjust index after erasing
+		closeConnections(index, "Connection closed by client");
 		return;
 	}
 	else
 	{
-		// if request too large
 		std::string receivedData(buffer, bytesReceived);
+		std::cout << "Received data before while: " << receivedData << std::endl;
 		bytesReceived = recv(fds[index].fd, buffer, sizeof(buffer), 0);
 		while (bytesReceived > 0)
 		{
@@ -49,19 +42,45 @@ void Manager::handleClientCommunication(size_t index)
 			receivedData.append(rest);
 			bytesReceived = recv(fds[index].fd, buffer, sizeof(buffer), 0);
 		}
+		std::cout << "Received data after while: " << receivedData << std::endl;
+		std::cout << "Received data size: " << receivedData.size() << std::endl;
 
-		if (receivedData.find("Content-Length:") != std::string::npos && bytesReceived != -1)
+		if (receivedData.find("Content-Length:") != std::string::npos)
 		{
+
 			std::string contentLength = receivedData.substr(receivedData.find("Content-Length:") + 16);
 			contentLength = contentLength.substr(0, contentLength.find("\r\n"));
-			size_t length = std::stoi(contentLength);
+			int length = std::stoi(contentLength);
 			bytesReceived = recv(fds[index].fd, buffer, sizeof(buffer), 0);
-			while (receivedData.size() < length)
+
+			size_t headerEndPos = receivedData.find("\r\n\r\n");
+			if (headerEndPos != std::string::npos)
 			{
+				// The header size is the position of the end of the header plus 4 (the length of "\r\n\r\n")
+				size_t headerSize = headerEndPos + 4;
+				// Subtract the header size from the length
+				length += headerSize;
+
+			}
+
+			while (bytesReceived < length)
+			{
+				if (bytesReceived == -1)
+				{
+					closeConnections(index, "Recv error: " + std::string(strerror(errno)));
+					break;
+				}
+				else if (bytesReceived == 0)
+				{
+					closeConnections(index, "Connection closed by client");
+					break;
+				}
 				std::string rest(buffer, bytesReceived);
 				receivedData.append(rest);
 				length -= bytesReceived;
 				bytesReceived = recv(fds[index].fd, buffer, sizeof(buffer), 0);
+																		printf("HI MOTHERFUCKER\n");
+			
 			}
 		}
 		if (receivedData.find("Expect:") != std::string::npos)
@@ -112,8 +131,8 @@ void Manager::handleClientCommunication(size_t index)
 			handleDelete(receivedData, fds, index);
 		}
 		else if ((receivedData.find("HEAD") != std::string::npos || receivedData.find("PUT") != std::string::npos ||
-				 receivedData.find("CONNECT") != std::string::npos || receivedData.find("OPTIONS") != std::string::npos ||
-				 receivedData.find("TRACE") != std::string::npos || receivedData.find("PATCH") != std::string::npos) &&
+				  receivedData.find("CONNECT") != std::string::npos || receivedData.find("OPTIONS") != std::string::npos ||
+				  receivedData.find("TRACE") != std::string::npos || receivedData.find("PATCH") != std::string::npos) &&
 				 receivedData.find("Content-Type:") == std::string::npos)
 		{
 			std::cout << "OTHER METHOD" << std::endl;
